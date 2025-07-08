8962b49f2902772f7b5bf74b161f5e3dd4bc72e0
Thunderwhere <brandonrshady@gmail.com>
2025-07-08 01:20:42 -0400

    Renames

--- VenueShare/Windows/ConfigWindow.cs
+++ VenueShare/Windows/ConfigWindow.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 
-namespace SamplePlugin.Windows;
+namespace VenueShare.Windows;
 
 public class ConfigWindow : Window, IDisposable
 {
